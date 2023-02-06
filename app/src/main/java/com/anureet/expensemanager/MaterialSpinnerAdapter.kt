69e4c7595a57c5896daa99c53dc1c610f4296fa0
Anureet Kaur <51269082+Anureet19@users.noreply.github.com>
2023-02-06 17:51:43 -0500

    code cleanup

--- app/src/main/java/com/anureet/expensemanager/MaterialSpinnerAdapter.kt
+++ app/src/main/java/com/anureet/expensemanager/MaterialSpinnerAdapter.kt
@@ -4,17 +4,6 @@ import android.content.Context
 import android.widget.ArrayAdapter
 import android.widget.Filter
 
-/**
- * Almost unbelievably, if we want to create a Material Spinner,
- * we're forced to subclass ArrayAdapter.  That statement alone
- * is maddening.  The issue is that there's no such thing as a Material
- * Spinner. Instead, there's an Exposed Dropdown Menu, which is really
- * an AutoCompleteTextView wrapped in a TextInputLayout, which replaces a
- * Spinner. The reason we have to subclass ArrayAdapter is because we need
- * the AutoCompleteTextView to act like a proper Spinner.  Thus we have to
- * override the AutoCompleteTextView's Filter so that it NEVER performs
- * filtering of the dropdown menu items.
- */
 class MaterialSpinnerAdapter<String>(context: Context, layout: Int, var values: MutableList<String>) :
     ArrayAdapter<String>(context, layout, values) {
     private val filter_that_does_nothing = object: Filter() {
